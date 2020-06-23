478ca5846770e10f7f58fb847af2f574be0ff143
sadfsdf <mamogaaa@yandex.ru>
2020-06-23 22:05:38 +0300

    fixes

--- testingapp/components/Motion.js
+++ testingapp/components/Motion.js
@@ -43,7 +43,7 @@ export default function Motion(props) {
       }}
       onPressOut={() => {
         setRecording(false);
-        props.onGesture(gesture);
+        props.onGesture([...gesture]);
       }}
     >
       <Text
--- testingapp/screens/HomeScreen.js
+++ testingapp/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
 import { useState } from 'react'
-import { Image, Platform, Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, Button, StyleSheet, Text, TouchableOpacity, View, ActivityIndicator } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 import { MonoText } from '../components/StyledText';
@@ -9,17 +9,42 @@ import Motion from "../components/Motion";
 
 import exampleImage from '../assets/images/11.jpg'
 
+import { train, predict, GestureData } from '../mobile_lib/index.js'
+import { apiUrl } from '../config';
+
 export default function HomeScreen() {
   const [result, setResult] = useState(null)
+  const [loading, setLoading] = useState(false)
+  if (loading) return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <ActivityIndicator size="large" color="black" />
+    </View>
+  );
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      { !result && <Motion onGesture={(gesture) => setResult(gesture)} /> }
-      { result && <View style={{ alignItems: "center" }}>
+      { !result && <Motion onGesture={async (gesture) => {
+        setLoading(true)
+        try {
+          let gestureData = new GestureData(gesture)
+          setResult(await predict(gestureData, apiUrl))
+          setLoading(false)
+        } catch (err) {
+          console.warn(err)
+          setLoading(false)
+        }
+      }} /> }
+      { result && result.name && <View style={{ alignItems: "center" }}>
         <Image
           style={{ width: 200, height: 200, marginBottom: 20 }}
-          source={exampleImage}
+          source={{
+            uri: result.image
+          }}
         />
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>I guess it's a circle!</Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>I guess it's a {result.name}!</Text>
+        <Button title="Reset" color="red" onPress={() => setResult(null)} />
+      </View> }
+      { result && !result.name && <View style={{ alignItems: "center" }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10, color: 'red' }}>Can't recognize gesture!</Text>
         <Button title="Reset" color="red" onPress={() => setResult(null)} />
       </View> }
     </View>
